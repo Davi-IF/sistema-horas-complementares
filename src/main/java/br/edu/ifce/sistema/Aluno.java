891498e1908e5f6a1caee6e1a2ae9efcc0a233f6
Davi-IF <p.davi.nunes@gmail.com>
2019-02-05 21:36:32 -0300

    Criação da classe Aluno e Adição da Lib JSTL

---
+++ src/main/java/br/edu/ifce/sistema/Aluno.java
@@ -0,0 +1,83 @@
+package br.edu.ifce.sistema;
+
+public class Aluno {
+	
+	private int id;
+	private String nome;
+	private String matricula;
+	private String cpf;
+	private String rg;
+	private int idade;
+	private String cep;
+	private String sexo;
+	private String curso;
+	private int semestre;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public int getSemestre() {
+		return semestre;
+	}
+	public void setSemestre(int semestre) {
+		this.semestre = semestre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
