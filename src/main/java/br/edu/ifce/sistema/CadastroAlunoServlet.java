34b49d4aba081c0d7c2ff7b5380c244b9f02cc51
Juliana <julyanapinheiro9@gmail.com>
2019-02-07 17:02:30 -0300

    Criação da Servlet Cadastro Aluno

--- src/main/java/br/edu/ifce/sistema/CadastroAlunoServlet.java
+++ src/main/java/br/edu/ifce/sistema/CadastroAlunoServlet.java
@@ -1,11 +1,16 @@
 package br.edu.ifce.sistema;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
+
 
 //Servlet Para Cadastramento do Aluno
 @WebServlet("/cadastroAluno")
@@ -14,7 +19,18 @@ public class CadastroAlunoServlet extends HttpServlet {
     
 	//Metodo doPost
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+System.out.println("Cadastrando novo Aluno");
 		
-	}
+		String nomeAluno = request.getParameter("nome");
+		Aluno aluno = new Aluno();
+		aluno.setNome(nomeAluno);
+		
+		Banco banco = new Banco();
+		banco.adiciona(aluno);
+		
+		request.setAttribute("aluno", aluno.getNome());
+		RequestDispatcher rd = request.getRequestDispatcher("/novoAlunoCriado.jsp");
+		rd.forward(request, response);
+	}	
 
 }
