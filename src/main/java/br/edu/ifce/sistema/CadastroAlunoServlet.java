42d18fd83357723ea3c584263b28b91223d1db97
Juliana <julyanapinheiro9@gmail.com>
2019-02-08 14:58:27 -0300

    atualizações na Servlet

--- src/main/java/br/edu/ifce/sistema/CadastroAlunoServlet.java
+++ src/main/java/br/edu/ifce/sistema/CadastroAlunoServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
+
 
 
 
@@ -25,12 +28,49 @@ System.out.println("Cadastrando novo Aluno");
 		Aluno aluno = new Aluno();
 		aluno.setNome(nomeAluno);
 		
+		String matricula = request.getParameter("matricula");
+		aluno.setMatricula(matricula);
+		
+		String cpf = request.getParameter("cpf");
+		aluno.setCpf(cpf);
+		
+		String rg = request.getParameter("rg");
+		aluno.setRg(rg);
+		
+		String cep = request.getParameter("cep");
+		aluno.setCep(cep);
+		
+		String endereco = request.getParameter("endereco");
+		aluno.setEndereco(endereco);
+		
+		String sexo = request.getParameter("sexo");
+		aluno.setSexo(sexo);
+		
+		String curso = request.getParameter("curso");
+		aluno.setCurso(curso);
+		
+		String email = request.getParameter("email");
+		aluno.setEmail(email);
+		
 		Banco banco = new Banco();
 		banco.adiciona(aluno);
 		
+		CPFValidator validator = new CPFValidator();
+		try {
+			// lógica de negócio ...
+			validator.assertValid(cpf);
+			
+			// continuação da lógica de negócio ...
+		} catch (InvalidStateException e) { // exception lançada quando o documento é inválido
+			
+		//Essa parte aqui que tou na duvida tem como me explicar 
+		//request.setAttribute("aluno", aluno.getNome());
+		RequestDispatcher rd = request.getRequestDispatcher("Aluno inválido");
+		//rd.forward(request, response);
+	}
 		request.setAttribute("aluno", aluno.getNome());
 		RequestDispatcher rd = request.getRequestDispatcher("/novoAlunoCriado.jsp");
 		rd.forward(request, response);
-	}	
 
+	}
 }
