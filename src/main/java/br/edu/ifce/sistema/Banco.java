87ab2d39dd609bb17b636c30906e540f71ea30fa
Juliana <julyanapinheiro9@gmail.com>
2019-02-09 17:50:14 -0300

    Correçao de erros e adicionando o banco

--- src/main/java/br/edu/ifce/sistema/Banco.java
+++ src/main/java/br/edu/ifce/sistema/Banco.java
@@ -1,10 +1,23 @@
 package br.edu.ifce.sistema;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Banco {
+	private static List<Aluno> listaDeAluno = new ArrayList<Aluno>();
 
 	public void adiciona(Aluno aluno) {
-		// TODO Auto-generated method stub
+		
+		listaDeAluno.add(aluno);
+	}
+	public List<Aluno>getCandidato(){
+		return Banco.listaDeAluno;
+	}
+		
+
+			
+			
 		
 	}
-
-}
